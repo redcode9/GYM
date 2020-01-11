30d98e47bdda54d94ad11cb0ed102d9cf6b57bb2
Pierfrancesco Sibilla <p.sibilla2@studenti.uniba.it>
2020-01-11 03:25:58 +0100

    svariate modifiche e fix vari in iscrizione collaboratore, tra cui aggiunta dei campi required, restrizione dei caratteri inseribili e generalizzazione del form pt.1 (campo ruolo che compare solo per il collaboratore)

--- resources/views/iscrizionecollaboratore2.blade.php
+++ resources/views/iscrizionecollaboratore2.blade.php
@@ -1,4 +1,11 @@
 @extends('base')
+<head>
+    <script language="javascript" type="text/javascript">
+        function removeSpaces(string) {
+            return string.trim();
+        }
+    </script>
+</head>
 @section('corpo')
     <div class="row mt-4">
         <div class="col-3"></div>
@@ -12,58 +19,41 @@
             <div class="row mt-5">
                 <h5 class="px-2"
                     style="margin-top: -0.75rem; margin-left: 4rem; color: dimgray; position:absolute; z-index: 2; background-color: white;">
-                    DETTAGLI
+                    ASSICURAZIONE
                 </h5></div>
             <form action="" method="post">
-                <div class="row ml-4 mr-4 mb-5">
-                    <div class="col-12 col-lg-12" style="padding: 2rem; border: 2px solid lightgray;">
-                        <div class="row mt-2">
-                            <div class="col-12 col-lg-6">
-                                <div class="form-group">
-                                    <label for="cf" class="mb-0"><h5>Codice Fiscale</h5></label>
-                                    <input type="text" class="form-control" name="cf" id="cf" value="">
-                                </div>
-                            </div>
-                            <div class="col-12 col-lg-6">
-                                <div class="form-group">
-                                    <label for="tel" class="mb-0"><h5>Cellulare</h5></label>
-                                    <input type="text" class="form-control" name="tel" id="tel" value="">
-                                </div>
-                            </div>
-                            <div class="col-12 col-lg-6 mt-3 mb-n2">
-                                <div class="form-group">
-                                    <label for="email" class="mb-0"><h5>Email</h5></label>
-                                    <input type="text" class="form-control" name="email" id="email" value="">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <h5 class="px-2"
-                        style="margin-top: -0.75rem; margin-left: 4rem; color: dimgray; position:absolute; z-index: 2; background-color: white;">
-                        ASSICURAZIONE
-                    </h5></div>
                 <div class="row ml-4 mr-4 mb-5 mt-n5">
                     <div class="col-12 col-lg-12 mt-5" style="padding: 2rem; border: 2px solid lightgray;">
                         <div class="row mt-2">
                             <div class="col-12 col-lg-6">
                                 <div class="form-group">
-                                    <label for="data1" class="mb-0"><h5>Data Stipula</h5></label>
-                                    <input type="text" class="form-control" name="data1" id="data1" value="">
+                                    <label for="numero" class="mb-0"><h5>Numero</h5></label>
+                                    <input type="text" pattern="[a-Z]" class="form-control" name="numero" id="numero"
+                                           value="" autofocus>
                                 </div>
                             </div>
-                            <div class="col-12 col-lg-6">
+                            <div class="col-12 col-lg-6"></div>
+                            <div class="col-12 col-lg-6 mt-3">
                                 <div class="form-group">
-                                    <label for="numero" class="mb-0"><h5>Numero</h5></label>
-                                    <input type="text" class="form-control" name="numero" id="numero" value="">
+                                    <label for="data1" class="mb-0"><h5>Data Stipula</h5></label>
+                                    <input type="date" pattern="\d{1,2}/\d{1,2}/\d{4}" class="form-control" name="data1"
+                                           id="data1" value="">
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-6 mt-3">
+                                <div class="form-group">
+                                    <label for="scadenza1" class="mb-0"><h5>Data Scadenza</h5></label>
+                                    <input type="date" pattern="\d{1,2}/\d{1,2}/\d{4}" class="form-control"
+                                           name="scadenza1" id="scadenza1" value="">
                                 </div>
                             </div>
                             <div class="col-12 col-lg-6 mt-3 mb-n2">
                                 <div class="form-group">
-                                    <label for="scadenza" class="mb-0"><h5>Data Scadenza</h5></label>
-                                    <input type="text" class="form-control" name="scadenza" id="scadenza" value="">
+                                    <label for="note1" class="mb-0"><h5>Note aggiuntive</h5></label>
+                                    <textarea type="text" pattern="[a-Z.]"
+                                              onfocus="this.value=removeSpaces(this.value);" class="form-control"
+                                              name="note1" id="note1" value="" style="resize: none" rows="3">
+                                    </textarea>
                                 </div>
                             </div>
                         </div>
@@ -81,13 +71,24 @@
                             <div class="col-12 col-lg-6 mb-n2">
                                 <div class="form-group">
                                     <label for="data2" class="mb-0"><h5>Data Rilascio</h5></label>
-                                    <input type="text" class="form-control" name="data2" id="data2" value="">
+                                    <input type="date" pattern="\d{1,2}/\d{1,2}/\d{4}" class="form-control" name="data2"
+                                           id="data2" value="">
                                 </div>
                             </div>
                             <div class="col-12 col-lg-6 mb-n2">
                                 <div class="form-group">
                                     <label for="scadenza2" class="mb-0"><h5>Data Scadenza</h5></label>
-                                    <input type="text" class="form-control" name="scadenza2" id="scadenza2" value="">
+                                    <input type="date" pattern="\d{1,2}/\d{1,2}/\d{4}" class="form-control"
+                                           name="scadenza2" id="scadenza2" value="">
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-6 mt-4 mb-n2">
+                                <div class="form-group">
+                                    <label for="note2" class="mb-0"><h5>Note aggiuntive</h5></label>
+                                    <textarea type="text" pattern="[a-Z.]"
+                                              onfocus="this.value=removeSpaces(this.value);" class="form-control"
+                                              name="note2" id="note2" value="" style="resize: none" rows="3">
+                                    </textarea>
                                 </div>
                             </div>
                         </div>
--- resources/views/iscrizionecollaboratore3.blade.php
+++ resources/views/iscrizionecollaboratore3.blade.php
@@ -20,20 +20,21 @@
                         <div class="row">
                             <div class="col-12 col-lg-6 mt-2">
                                 <div class="form-group">
-                                    <label for="data" class="mb-0"><h5>Data Tesseramento</h5></label>
-                                    <input type="text" class="form-control" name="data" id="data" value="">
+                                    <label for="numero" class="mb-0"><h5>Numero Tessera*</h5></label>
+                                    <input type="text" class="form-control" name="numero" id="numero" value="" required>
                                 </div>
                             </div>
-                            <div class="col-12 col-lg-6 mt-2">
+                            <div class="col-12 col-lg-6 mt-2"></div>
+                            <div class="col-12 col-lg-6 mt-3">
                                 <div class="form-group">
-                                    <label for="numero" class="mb-0"><h5>Numero Tessera</h5></label>
-                                    <input type="text" class="form-control" name="numero" id="numero" value="">
+                                    <label for="data" class="mb-0"><h5>Data Tesseramento*</h5></label>
+                                    <input type="text" class="form-control" name="data" id="data" value="" required>
                                 </div>
                             </div>
                             <div class="col-12 col-lg-6 mt-3 mb-n2">
                                 <div class="form-group">
-                                    <label for="scadenza" class="mb-0"><h5>Scadenza</h5></label>
-                                    <input type="text" class="form-control" name="scadenza" id="scadenza" value="">
+                                    <label for="scadenza" class="mb-0"><h5>Scadenza Tessera*</h5></label>
+                                    <input type="text" class="form-control" name="scadenza" id="scadenza" value="" required>
                                 </div>
                             </div>
                         </div>
