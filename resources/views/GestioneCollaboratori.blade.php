8d479bde9918f1b901c214f76335fa766aed8b00
Piero <p.carrieri6@studenti.uniba.it>
2020-01-16 21:01:25 +0100

    Aggiornamento alle view Gestione "Qualcosa"

--- resources/views/GestioneCollaboratori.blade.php
+++ resources/views/GestioneCollaboratori.blade.php
@@ -1,3 +1,6 @@
-@component('templates.TabelleSTD')
-    @slot('title_upper_table','Gestione Collaboratori')   <!-- Se si vuole inserire un dato interattivo, seguire il seguente form-->
+@component('templates.TabelleSTD',['SearchBox' => "OFF"])  <!-- Cambiare in OFF se la searchbox non serve in questa tabella-->
+<!-- ciao-->
+
+@slot('title_upper_view','Gestione Utenti')
+@slot('title_upper_table','Gestione Collaboratori')   <!-- Se si vuole inserire un dato interattivo, seguire il seguente form-->
 @endcomponent
