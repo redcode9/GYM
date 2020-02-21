c123226cc2477f6bafa6b1a53dbfb6c51ec37b66
Alessia Calia <alessia74100@gmail.com>
2020-02-22 00:33:05 +0100

    modifica pagina Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -21,7 +21,10 @@
     <div class="row">
         <div class="col-3"></div>
         <div class="col-6">
-            <div class="jumbotron mt-5" style="background-color: #ebf8ff">
+            <div>
+                <p style="text-align: center"><img style="width: 30%;" src="{{asset('img/logo_gym3.png')}}" alt="Logo">
+                </p></div>
+            <div class="jumbotron pb-3 pt-3" style="background-color: #fae8c6">
                 <div class="row">
                     <div class="col-12">
                         <h1 class="text-center">Login</h1>
@@ -36,7 +39,7 @@
                                 <form method="post" action="{{route("login")}}">
                                     <div class="input-group form-group">
                                         <div class="input-group-prepend">
-                                            <span class="input-group-text" style="background-color: #b6e7ff"><i class="fas fa-user"></i></span>
+                                            <span class="input-group-text" style="background-color: #ffac14"><i class="fas fa-user"></i></span>
                                         </div>
                                         <input type="text" name="username" class="form-control" placeholder="Username">
 
@@ -44,12 +47,15 @@
 
                                     <div class="input-group">
                                         <div class="input-group-prepend">
-                                            <span class="input-group-text" style="background-color: #b6e7ff"><i class="fas fa-key icon"></i></span>
+                                            <span class="input-group-text" style="background-color: #ffac14"><i class="fas fa-key icon"></i></span>
                                         </div>
                                         <input type="Password" name="password" class="form-control" placeholder="Password"/>
                                     </div><br />
-                                    <div class="form-group">
+                                    <div class="form-group" >
                                         <input class="btn btn-block btn-dark" type="submit" name="submit" value="Accedi">
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="btn btn-block btn-dark" type="button" name="button" value="Registra ASD">
                                     </div>
                                     @csrf
 
