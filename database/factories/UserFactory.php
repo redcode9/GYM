43e62a89368a1304c27acde6d9f764725fbd081e
Joseph Tortorella <j.tortorella@studenti.uniba.it>
2020-02-24 22:41:52 +0100

    Aggiunto seeding per l'utente admin

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,8 +1,9 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/** @var Factory $factory */
 use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 use Illuminate\Support\Str;
 
 /*
@@ -18,10 +19,9 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
+        'username' => 'admin',
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'ruolo' => 'admin',
         'remember_token' => Str::random(10),
     ];
 });
