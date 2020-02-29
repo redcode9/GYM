cb3df0299424a08d2f4436b114e9373849d06b2a
Antonio M. Savino <antony14294@gmail.com>
2020-02-29 23:24:43 +0100

    Aggiunta iscrizione socio a corso (Backend, no transazione)

--- app/Http/Controllers/IscrizioneSocioController.php
+++ app/Http/Controllers/IscrizioneSocioController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Associazione;
+use App\Corso;
 use App\DatiFiscali;
 use App\Iscritto;
+use App\Partecipazione;
 use App\Sala;
 use App\Socio;
 use App\Transazione;
@@ -17,7 +19,8 @@ use Illuminate\Support\Facades\Auth;
 class IscrizioneSocioController extends Controller
 {
 
-    public function registrazione(Request $request){
+    public function registrazione(Request $request)
+    {
 
         if ($request->has("nome")) {
             $iscritto = new Iscritto;
@@ -28,14 +31,14 @@ class IscrizioneSocioController extends Controller
             $socio->luogo_nasc = $request->input("nascita");
             $socio->data_nasc = $request->input("data");
 
-            if($request->tipoSocio == 'option1'){
+            if ($request->tipoSocio == 'option1') {
                 $socio->tipo = 'allievo';
-            } elseif ($request->tipoSocio == 'option2'){
+            } elseif ($request->tipoSocio == 'option2') {
                 $socio->tipo = 'insegnante';
             }
 
-            $socio->verbalizzato=0;
-            $socio->fondatore=0;
+            $socio->verbalizzato = 0;
+            $socio->fondatore = 0;
 
             $iscritto->indirizzo = $request->input("indirizzo");
             $iscritto->cap = $request->input("cap");
@@ -60,24 +63,25 @@ class IscrizioneSocioController extends Controller
             ]);
 
         }
-    /*} else {
-            echo "ciao ".Auth::id()." ciao";
-        }*/
+        /*} else {
+                echo "ciao ".Auth::id()." ciao";
+            }*/
     }
 
 
-    public function registrazione2($id, Request $request){
+    public function registrazione2($id, Request $request)
+    {
 
-        if($request->has("data1")){
+        if ($request->has("data1")) {
             $socio = Socio::find($id);
-            $iscritto  = Iscritto::find($socio->iscrizione);
+            $iscritto = Iscritto::find($socio->iscrizione);
             //data1 numero scadenza
             $iscritto->ass_rilascio = $request->get("data1");
             $iscritto->ass_scad = $request->get("scadenza1");
             $iscritto->ass_num = $request->get("numero");
             //data2 scadenza2
-            $iscritto->cert_med_scadenza =  $request->get("scadenza2");
-            $iscritto->cert_med_rilascio =  $request->get("data2");
+            $iscritto->cert_med_scadenza = $request->get("scadenza2");
+            $iscritto->cert_med_rilascio = $request->get("data2");
 
             $iscritto->save();
 
@@ -91,15 +95,16 @@ class IscrizioneSocioController extends Controller
         }
     }
 
-    public function registrazione3($id, Request $request){
+    public function registrazione3($id, Request $request)
+    {
 
-        if($request->has("numero")){
+        if ($request->has("numero")) {
             $socio = Socio::find($id);
             $tessera = new Tessera;
-            $tessera->data_tess =  $request->get("emissione");
-            $tessera->scad_tess =  $request->get("scadenza");
+            $tessera->data_tess = $request->get("emissione");
+            $tessera->scad_tess = $request->get("scadenza");
             $tessera->save();
-            $socio->tessera  = $tessera->id;
+            $socio->tessera = $tessera->id;
             $socio->save();
 
             //dati fiscali
@@ -108,9 +113,9 @@ class IscrizioneSocioController extends Controller
             $datifiscali = new DatiFiscali;
             $datifiscali->pagamento = $request->get("pagamento");
             $datifiscali->descrizione = $request->get("descrizione");
-                //$datidiscali->importo = $request->get("importo");
-                $datifiscali->tipo_doc = $request->get("tipoDocumento");
-                $datifiscali->data_iscriz = date("Y-m-d");
+            //$datidiscali->importo = $request->get("importo");
+            $datifiscali->tipo_doc = $request->get("tipoDocumento");
+            $datifiscali->data_iscriz = date("Y-m-d");
             $datifiscali->socio = $socio->id;
             $datifiscali->save();
             $socio->dati_fiscali = $datifiscali->id;
@@ -124,26 +129,26 @@ class IscrizioneSocioController extends Controller
 
 
         } else {
-            $dati = DatiFiscali::where("tipo_doc","F")->count();
+            $dati = DatiFiscali::where("tipo_doc", "F")->count();
             $ass = Associazione::all()->first();
             //$imp = $ass->importo;
             //for test
             $n_tess = Tessera::max("id");
             $n_tess += 1;
             $imp = 20;
-            $fattura = DatiFiscali::where("tipo_doc","F")->count()+1;
-            $ricevutaN = DatiFiscali::where("tipo_doc","RN")->count()+1;
-            $ricevuta = DatiFiscali::where("tipo_doc","R")->count()+1;
-            $ricevutaF= DatiFiscali::where("tipo_doc","RF")->count()+1;
+            $fattura = DatiFiscali::where("tipo_doc", "F")->count() + 1;
+            $ricevutaN = DatiFiscali::where("tipo_doc", "RN")->count() + 1;
+            $ricevuta = DatiFiscali::where("tipo_doc", "R")->count() + 1;
+            $ricevutaF = DatiFiscali::where("tipo_doc", "RF")->count() + 1;
 
             return view("iscrizionesocio3", [
                 "errore" => "",
                 "socio" => $id,
                 "n_tess" => $n_tess,
-                "F"=> $fattura,
-                "RN"=> $ricevutaN,
-                "R"=> $ricevuta,
-                "RF"=> $ricevutaF,
+                "F" => $fattura,
+                "RN" => $ricevutaN,
+                "R" => $ricevuta,
+                "RF" => $ricevutaF,
                 "importo" => $imp
             ]);
         }
@@ -152,72 +157,45 @@ class IscrizioneSocioController extends Controller
     public function registrazione4($id, Request $request)
     {
 
-            $socio = Socio::find($id);
-            $sale = Sala::all();
+        $socio = Socio::find($id);
+        $sale = Sala::all();
 
-            $years = Carbon::parse($socio->data_nasc)->age; //calcola l'età dell'iscritto
+        $years = Carbon::parse($socio->data_nasc)->age; //calcola l'età dell'iscritto
 
-            if ($request->isMethod("POST")){
-                return redirect()->route('HomeAdmin');
-            } else {
-
-                return view("iscrizionesocio4", [
-                    "errore" => "",
-                    "sale" => $sale,
-                    "socio" => $socio,
-                    "years" => $years
-                ]);
+        if ($request->isMethod("POST")) {
+            $selezione = $request->post("group");
+            foreach ($selezione as $sel){
+                $part = new Partecipazione();
+                $part->allievo = $id;
+                $part->corso = $sel;
+                $part->save();
             }
+            return redirect()->route('HomeAdmin');
+        } else {
+
+            return view("iscrizionesocio4", [
+                "errore" => "",
+                "sale" => $sale,
+                "socio" => $socio,
+                "years" => $years
+            ]);
+        }
     }
 
+    public function stampaModuloMagg()
+    {
 
+        $path = '..\resources\modulistica\moduli_maggiorenne.pdf';
+        return response()->download($path);
 
+    }
 
+    public function stampaModuloMin()
+    {
 
-    //public function tiposocio($id, Request $request){
-//
-    //    if($request->isMethod("POST")){
-    //        $socio = Socio::find($id);
-    //        $socio->verbalizzato=0;
-    //        $socio->save();
-    //        $scelta = $request->get("direzione");
-    //        switch ($scelta){
-    //            case "termina":
-    //                return redirect()->route("utenti",["id"=>$id]);
-    //                break;
-    //            case "corsi":
-    //                return redirect()->route("corsi",["id"=>$id]);
-    //        }
-//
-//
-    //    } else {
-//
-    //        return view("tiposocio", [
-    //            "errore" => "",
-    //            "socio" => $id
-    //        ]);
-    //    }
-    //}
+        $path = '..\resources\modulistica\moduli_minorenne.pdf';
+        return response()->download($path);
 
-
-
-//ciao
-
-
-        public function stampaModuloMagg()
-        {
-
-            $path = '..\resources\modulistica\moduli_maggiorenne.pdf';
-            return response()->download($path);
-
-        }
-
-        public function stampaModuloMin()
-        {
-
-            $path = '..\resources\modulistica\moduli_minorenne.pdf';
-            return response()->download($path);
-
-        }
+    }
 
 }
